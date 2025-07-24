04f6484b396122ebd6ea16965e1f27c8861adaea
1-MadanMohan <madanmohan1507330@gmail.com>
2025-07-24 13:58:59 +0530

    cookies,bearer(header) authentication

--- 5Shortner/controllers/user.js
+++ 5Shortner/controllers/user.js
@@ -1,81 +1,39 @@
-const User = require("../models/user");
-const { setUser } = require("../service/auth"); // setUser returns JWT
+  const User = require("../models/user");
+  const { setUser } = require("../service/auth"); // setUser returns JWT
 
-async function handleUserSignup(req, res) {
-  const { name, email, password } = req.body;
+  async function handleUserSignup(req, res) {
+    const { name, email, password } = req.body;
 
-  try {
-    await User.create({ name, email, password });
-    return res.redirect("/login"); // after signup, redirect to login
-  } catch (err) {
-    console.error("Signup error:", err);
-    return res.render("signup", { error: "User already exists or invalid input" });
-  }
-}
-
-async function handleUserLogin(req, res) {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email, password }); // In production, use hashed password check
-
-  if (!user) {
-    return res.render("login", {
-      error: "Invalid Username or Password",
-    });
+    try {
+      await User.create({ name, email, password });
+      return res.redirect("/login"); // after signup, redirect to login
+    } catch (err) {
+      console.error("Signup error:", err);
+      return res.render("signup", { error: "User already exists or invalid input" });
+    }
   }
 
-  const token = setUser({ _id: user._id, email: user.email });
+  async function handleUserLogin(req, res) {
+    const { email, password } = req.body;
 
-  res.cookie("uid", token, {
-    httpOnly: true,     // prevent JS access (XSS protection)
-    secure: false,      // true if using HTTPS
-  });
+    const user = await User.findOne({ email, password }); // In production, use hashed password check
 
-  return res.redirect("/");
-}
+    if (!user) {
+      return res.render("login", {
+        error: "Invalid Username or Password",
+      });
+    }
 
-module.exports = {
-  handleUserSignup,
-  handleUserLogin,
-};
+    const token = setUser({ _id: user._id, email: user.email });
 
 
 
+    return res.json({token});
+  }
+
+  module.exports = {
+    handleUserSignup,
+    handleUserLogin,
+  };
 
 
-
-
-
-// const { v4: uuidv4 } = require("uuid");
-// const User = require("../models/user");
-// const { setUser } = require("../service/auth");
-
-// async function handleUserSignup(req, res) {
-//   const { name, email, password } = req.body;
-//   await User.create({
-//     name,
-//     email,
-//     password,
-//   });
-//   return res.redirect("/");
-// }
-
-// async function handleUserLogin(req, res) {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email, password });
-
-//   if (!user)
-//     return res.render("login", {
-//       error: "Invalid Username or Password",
-//     });
-
-//   const sessionId = uuidv4();
-//   setUser(sessionId, user);
-//   res.cookie("uid", sessionId);
-//   return res.redirect("/");
-// }
-
-// module.exports = {
-//   handleUserSignup,
-//   handleUserLogin,
-// };
--- 5Shortner/middlewares/auth.js
+++ 5Shortner/middlewares/auth.js
@@ -1,21 +1,27 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
-
+  const userUid = req.headers["authorization"]
+// console.log(req.headers);
   if (!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+  const token = userUid.split("Bearer ")[1];
+  const user = getUser(token);
 
-  if (!user) return res.redirect("/login");
+
+  if (!user) return res.redirect("/login"); 
 
   req.user = user;
   next();
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
 
-  const user = getUser(userUid);
+  const userUid = req.headers["authorization"]
+console.log(req.headers);
+  const token = userUid.split("Bearer ")[1];
+
+
+  const user = getUser(token);
 
   req.user = user;
   next();
